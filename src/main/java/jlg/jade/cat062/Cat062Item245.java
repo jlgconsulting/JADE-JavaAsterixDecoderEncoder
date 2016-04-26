cf8ce06c0062480067ba6aed21ff482f744d7f50
Dan Geabunea <dan.geabunea@jlg.ro>
2016-04-26 13:45:50 +0300

    Finished decoding implementation of Cat062 Item 245;

---
+++ src/main/java/jlg/jade/cat062/Cat062Item245.java
@@ -0,0 +1,40 @@
+/* 
+* Created by dan-geabunea on 4/26/2016.
+* This code is the property of JLG Consulting. Please
+* check the license terms for this product to see under what
+* conditions you can use or modify this source code.
+*/
+package jlg.jade.cat062;
+
+import jlg.jade.abstraction.FixedLengthAsterixItem;
+import jlg.jade.asterix.AsterixItemLength;
+
+/**
+ * Cat 062 Item 245 - Target (aircraft or vehicle) identification in 8 characters.
+ * @implSpec  Item is not used any more according to Asterix ICD
+ */
+public class Cat062Item245 extends FixedLengthAsterixItem {
+
+
+    @Override
+    protected int decodeFromByteArray(byte[] input, int offset) {
+        /**
+         * @implNote
+         * This item is not used anymore according to Asterix ICD (pg 39 v1.12). So, if it is present in the data,
+         * we shall just increment the offset, without decoding.
+         */
+
+        appendDebugMsg("Item is not used anymore. Skipping the decoding of data.");
+        return offset + this.getSizeInBytes();
+    }
+
+    @Override
+    protected boolean validate() {
+        return true;
+    }
+
+    @Override
+    protected int setSizeInBytes() {
+        return AsterixItemLength.SEVEN_BYTES.getValue();
+    }
+}
---
+++ src/test/java/jlg/jade/test/asterix/Cat062Item245Test.java
@@ -0,0 +1,42 @@
+/* 
+* Created by dan-geabunea on 4/26/2016.
+* This code is the property of JLG Consulting. Please
+* check the license terms for this product to see under what
+* conditions you can use or modify this source code.
+*/
+package jlg.jade.test.asterix;
+
+import jlg.jade.cat062.Cat062Item245;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class Cat062Item245Test {
+
+    /**
+     * This item is not used any more, so decoding it is not necessary. The only impl it has
+     * is to increase the offset, without actually decoding anything.
+     */
+
+    @Test
+    public void should_have_length_of_7_bytes(){
+        //arrange
+        Cat062Item245 item = new Cat062Item245();
+
+        //assert
+        assertEquals("Item length is invalid", 7, item.getSizeInBytes());
+    }
+
+    @Test
+    public void the_decode_method_should_increase_offset_by_7(){
+        //arrange
+        byte[] input = {1,2,3,4,5,6,7};
+        Cat062Item245 item = new Cat062Item245();
+        int offset = 0;
+
+        //act
+        int result = item.decode(input, offset);
+
+        //assert
+        assertEquals("Offset not incremented after data decoding", offset + item.getSizeInBytes(), result);
+    }
+}
