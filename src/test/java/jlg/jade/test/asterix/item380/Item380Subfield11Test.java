0a23f3de09952fba88516b16f3a9d1a7a5778411
Dan Geabunea <dan.geabunea@jlg.ro>
2016-04-28 14:14:15 +0300

    Implemented decoding of Item380 Subfield 11;

---
+++ src/test/java/jlg/jade/test/asterix/item380/Item380Subfield11Test.java
@@ -0,0 +1,57 @@
+/* 
+* Created by dan-geabunea on 4/28/2016.
+* This code is the property of JLG Consulting. Please
+* check the license terms for this product to see under what
+* conditions you can use or modify this source code.
+*/
+package jlg.jade.test.asterix.item380;
+
+import jlg.jade.cat062.item380.Item380Subfield11;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+
+public class Item380Subfield11Test implements BasicAsterixDataTestSuite {
+
+    @Override
+    @Test
+    public void should_have_length_correct_length() {
+        //arrange
+        Item380Subfield11 subfield = new Item380Subfield11();
+
+        //assert
+        assertEquals("Invalid item length", 2, subfield.getSizeInBytes());
+    }
+
+    @Override
+    @Test
+    public void the_decode_method_should_increment_offset_after_decoding_data() {
+        //arrange
+        byte[] input = {1,2};
+        int offset = 0;
+        Item380Subfield11 subfield = new Item380Subfield11();
+
+        //act
+        int result = subfield.decode(input,offset,input.length);
+
+        //assert
+        assertEquals("Offset not incremented after decoding of data", 2, result);
+    }
+
+    @Override
+    @Test
+    public void the_decode_method_should_set_valid_flag_to_true() {
+        //arrange
+        byte[] input = {1,2};
+        int offset = 0;
+        Item380Subfield11 subfield = new Item380Subfield11();
+
+        //act
+        subfield.decode(input,offset,input.length);
+
+        //assert
+        assertTrue(subfield.isValid());
+    }
+}
+
