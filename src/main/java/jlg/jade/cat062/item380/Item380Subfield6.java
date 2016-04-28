f5a3776694ba9dbb67a4aca51cae5146108247cc
Dan Geabunea <dan.geabunea@jlg.ro>
2016-04-28 09:06:20 +0300

    Implemented decoding of Item 380 Subfield 6

---
+++ src/main/java/jlg/jade/cat062/item380/Item380Subfield6.java
@@ -0,0 +1,44 @@
+/* 
+* Created by dan-geabunea on 4/28/2016.
+* This code is the property of JLG Consulting. Please
+* check the license terms for this product to see under what
+* conditions you can use or modify this source code.
+*/
+package jlg.jade.cat062.item380;
+
+import jlg.jade.abstraction.AsterixSubfield;
+import jlg.jade.asterix.AsterixItemLength;
+
+/**
+ * Item 380 Subfield #6 - Selected altitude
+ * The short-term vertical intent as described by either the FMS
+ * selected altitude, the Altitude Control Panel Selected Altitude
+ * (FCU/MCP), or the current aircraft altitude according to the
+ * aircraft's mode of flight.
+ * @implNote This item is not currently needed, so it will not be decoded
+ */
+public class Item380Subfield6 extends AsterixSubfield {
+    @Override
+    protected int setSizeInBytes() {
+        return AsterixItemLength.TWO_BYTES.getValue();
+    }
+
+    @Override
+    protected int decodeFromByteArray(byte[] input, int offset) {
+
+        /**
+         * @implNote
+         * This subfield is not decoded because it is not needed yet.
+         * Will only increase offset.
+         */
+
+        appendDebugMsg("Subfield decoding not implemented. Skipping this subfield.");
+
+        return offset + this.sizeInBytes;
+    }
+
+    @Override
+    protected boolean validate() {
+        return true;
+    }
+}
