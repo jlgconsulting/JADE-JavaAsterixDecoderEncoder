c42baab26f5ae970380f059f61eca6e80100659c
Dan Geabunea <dan.geabunea@jlg.ro>
2016-05-04 08:28:40 +0300

    Implemented decoding for item 380 subfield 26 - indicated air speed

---
+++ src/test/java/jlg/jade/test/asterix/item380/Item380Subfield26Test.java
@@ -0,0 +1,77 @@
+/* 
+* Created by dan-geabunea on 5/4/2016.
+* This code is the property of JLG Consulting. Please
+* check the license terms for this product to see under what
+* conditions you can use or modify this source code.
+*/
+package jlg.jade.test.asterix.item380;
+
+import jlg.jade.cat062.item380.Item380Subfield26;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(JUnitParamsRunner.class)
+public class Item380Subfield26Test implements BasicAsterixDataTestSuite {
+    @Override
+    @Test
+    public void should_have_length_correct_length() {
+        //arrange
+        Item380Subfield26 subfield = new Item380Subfield26();
+
+        //assert
+        assertEquals("Subfield length is invalid", 2, subfield.getSizeInBytes());
+    }
+
+    @Override
+    @Test
+    public void the_decode_method_should_increment_offset_after_decoding_data() {
+        //arrange
+        byte[] input = {1, 8};
+        int offset = 0;
+        Item380Subfield26 subfield = new Item380Subfield26();
+
+        //act
+        int result = subfield.decode(input, offset, input.length);
+
+        //assert
+        assertEquals("Offset not incremented after data decoding", offset + 2, result);
+    }
+
+    @Test
+    public void the_decode_method_should_correctly_decode_data() {
+        //arrange
+        byte[] input = {1, 8};
+        int offset = 0;
+        Item380Subfield26 subfield = new Item380Subfield26();
+
+        //act
+        subfield.decode(input, offset, input.length);
+
+        //assert
+        assertEquals("Indicated air speed not decoded correctly", 264, subfield.getIndicatedAirSpeed());
+    }
+
+    @Test
+    @Parameters({"0,0,true", "4,76,true", "4,77,false"})
+    public void the_validate_method_should_correctly_set_valid_flag(int firstOctet, int secondOctet, boolean expected) {
+        /**
+         * @implNote
+         * Validation: 0 Kt ≤ Indicated Airspeed ≤ 1100 Kt
+         */
+
+        //arrange
+        byte[] input = {(byte) firstOctet, (byte) secondOctet};
+        int offset = 0;
+        Item380Subfield26 subfield = new Item380Subfield26();
+
+        //act
+        subfield.decode(input, offset, input.length);
+
+        //assert
+        assertEquals(expected, subfield.isValid());
+    }
+}
