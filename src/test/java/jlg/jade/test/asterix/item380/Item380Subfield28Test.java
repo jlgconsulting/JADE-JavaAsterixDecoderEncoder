2cab4bbe8bc23f3d69e93b58328ba3e8ba59a4ba
Dan Geabunea <dan.geabunea@jlg.ro>
2016-05-04 09:15:48 +0300

    Implemented decoding of item 380 subfield 28

---
+++ src/test/java/jlg/jade/test/asterix/item380/Item380Subfield28Test.java
@@ -0,0 +1,39 @@
+/* 
+* Created by dan-geabunea on 5/4/2016.
+* This code is the property of JLG Consulting. Please
+* check the license terms for this product to see under what
+* conditions you can use or modify this source code.
+*/
+package jlg.jade.test.asterix.item380;
+
+import jlg.jade.cat062.item380.Item380Subfield28;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Item380Subfield28Test implements BasicAsterixDataTestSuite {
+    @Override
+    @Test
+    public void should_have_length_correct_length() {
+        //arrange
+        Item380Subfield28 subfield = new Item380Subfield28();
+
+        //assert
+        assertEquals("Subfield length is invalid", 2, subfield.getSizeInBytes());
+    }
+
+    @Override
+    @Test
+    public void the_decode_method_should_increment_offset_after_decoding_data() {
+        //arrange
+        byte input[] = {1, 2};
+        int offset = 0;
+        Item380Subfield28 subfield = new Item380Subfield28();
+
+        //act
+        int result = subfield.decode(input, offset, input.length);
+
+        //assert
+        assertEquals("Offset not incremented after decoding data", offset + 2, result);
+    }
+}
