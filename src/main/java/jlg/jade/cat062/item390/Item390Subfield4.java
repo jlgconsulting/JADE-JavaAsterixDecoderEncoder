3ecd1a0e82248395eba4b8fa98347f46f960c4b4
Dan Geabunea <dan.geabunea@jlg.ro>
2016-05-10 15:47:41 +0300

    Imploemented decoding for cat 062 item 390 subfield 4 - flight category

---
+++ src/main/java/jlg/jade/cat062/item390/Item390Subfield4.java
@@ -0,0 +1,111 @@
+/* 
+* Created by dan-geabunea on 5/10/2016.
+* This code is the property of JLG Consulting. Please
+* check the license terms for this product to see under what
+* conditions you can use or modify this source code.
+*/
+package jlg.jade.cat062.item390;
+
+import jlg.jade.abstraction.FixedLengthAsterixData;
+import jlg.jade.asterix.AsterixItemLength;
+
+import java.util.BitSet;
+
+/**
+ * Item 390 - Subfield 4 - Flight category
+ * Contains info about traffic type, instrumentaiton, rvsm and priority.
+ */
+public class Item390Subfield4 extends FixedLengthAsterixData {
+    private int trafficType;
+    private int flightRules;
+    private int rvsm;
+    private int priority;
+
+    @Override
+    protected int setSizeInBytes() {
+        return AsterixItemLength.ONE_BYTE.getValue();
+    }
+
+    @Override
+    protected void decodeFromByteArray(byte[] input, int offset) {
+        BitSet bs = BitSet.valueOf(new byte[]{input[offset]});
+
+        //decode traffic type
+        this.trafficType = decodeFromTwoBits(bs, 6, 7);
+        appendItemDebugMsg("Traffic type (0=Unk,1=GAT,2=OAT,3=NA)", this.trafficType);
+
+        //decode flight rules
+        this.flightRules = decodeFromTwoBits(bs, 4, 5);
+        appendItemDebugMsg("Flight rules (0=IFR,1=VFR,2=NA,3=Ctrl. VFR)", this.flightRules);
+
+        //decode RVSM
+        this.rvsm = decodeFromTwoBits(bs, 2, 3);
+        appendItemDebugMsg("RVSM (0=Unk,1=Approved,2=Exempt,3=Not approved)", this.rvsm);
+
+        //decode HPR
+        if (bs.get(0)) {
+            this.priority = 1;
+        } else {
+            this.priority = 0;
+        }
+        appendItemDebugMsg("Flight priority (0=Normal,1=High)", this.priority);
+
+    }
+
+    /**
+     * @return The traffic type of the aircraft
+     * - 0 is Unknown
+     * - 1 is GAT
+     * - 2 is OAT
+     * - 3 is NA
+     */
+    public int getTrafficType() {
+        return trafficType;
+    }
+
+    /**
+     * @return The flight rules
+     * - 0 is IFR
+     * - 1 is VFR
+     * - 2 is NA
+     * - 3 is CVFR
+     */
+    public int getFlightRules() {
+        return flightRules;
+    }
+
+    /**
+     * @return The RVSM info
+     * - 0 is Unknown
+     * - 1 is Approved
+     * - 2 is Exempt
+     * - 3 is Not approved
+     */
+    public int getRvsm() {
+        return rvsm;
+    }
+
+    /**
+     * @return The priority of the flight
+     * - 0 is Normal flight
+     * - 1 is High priority flight
+     */
+    public int getPriority() {
+        return priority;
+    }
+
+    private int decodeFromTwoBits(BitSet bs, int startBitIndex, int endBitIndex) {
+        final boolean startBit = bs.get(startBitIndex);
+        final boolean endBit = bs.get(endBitIndex);
+        if (!endBit && !startBit) {
+            return 0;
+        }
+        if (!endBit && startBit) {
+            return 1;
+        }
+        if (endBit && !startBit) {
+            return 2;
+        }
+        return 3;
+    }
+}
---
+++ src/test/java/jlg/jade/test/asterix/item390/Item390Subfield4Test.java
@@ -0,0 +1,105 @@
+/* 
+* Created by dan-geabunea on 5/10/2016.
+* This code is the property of JLG Consulting. Please
+* check the license terms for this product to see under what
+* conditions you can use or modify this source code.
+*/
+package jlg.jade.test.asterix.item390;
+
+import jlg.jade.cat062.item390.Item390Subfield4;
+import jlg.jade.test.utils.MandatoryFixedLengthAsterixTests;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(JUnitParamsRunner.class)
+public class Item390Subfield4Test extends MandatoryFixedLengthAsterixTests<Item390Subfield4> {
+    @Override
+    protected int setExpectedItemSizeInBytes() {
+        return 1;
+    }
+
+    @Override
+    protected Item390Subfield4 setFixedLengthAsterixDataInstance() {
+        return new Item390Subfield4();
+    }
+
+    @Test
+    @Parameters({"0,0", "64,1", "128,2", "192,3"})
+    public void the_decode_method_should_correctly_decode_traffic_type(int firstOctet, int expected) {
+        //arrange
+        byte[] input = {(byte) firstOctet};
+        int offset = 0;
+        Item390Subfield4 subfield4 = new Item390Subfield4();
+
+        //act
+        subfield4.decode(input, offset, input.length);
+
+        //assert
+        assertEquals("I062/390 #4 Traffic type not decoded correctly", expected, subfield4.getTrafficType());
+    }
+
+    @Test
+    @Parameters({"0,0", "16,1", "32,2", "48,3"})
+    public void the_decode_method_should_correctly_decode_flight_rules(int firstOctet, int expected) {
+        //arrange
+        byte[] input = {(byte) firstOctet};
+        int offset = 0;
+        Item390Subfield4 subfield4 = new Item390Subfield4();
+
+        //act
+        subfield4.decode(input, offset, input.length);
+
+        //assert
+        assertEquals("I062/390 #4 Flight rules not decoded correctly", expected, subfield4.getFlightRules());
+    }
+
+    @Test
+    @Parameters({"0,0", "4,1", "8,2", "12,3"})
+    public void the_decode_method_should_correctly_decode_rvsm(int firstOctet, int expected) {
+        //arrange
+        byte[] input = {(byte) firstOctet};
+        int offset = 0;
+        Item390Subfield4 subfield4 = new Item390Subfield4();
+
+        //act
+        subfield4.decode(input, offset, input.length);
+
+        //assert
+        assertEquals("I062/390 #4 RVSM not decoded correctly", expected, subfield4.getRvsm());
+    }
+
+    @Test
+    @Parameters({"0,0", "1,1"})
+    public void the_decode_method_should_correctly_decode_hpr(int firstOctet, int expected) {
+        //arrange
+        byte[] input = {(byte) firstOctet};
+        int offset = 0;
+        Item390Subfield4 subfield4 = new Item390Subfield4();
+
+        //act
+        subfield4.decode(input, offset, input.length);
+
+        //assert
+        assertEquals("I062/390 #4 HPR not decoded correctly", expected, subfield4.getPriority());
+    }
+
+    @Test
+    public void the_decode_method_should_correclty_decode_all_data() {
+        byte[] input = {64};
+        int offset = 0;
+        Item390Subfield4 subfield4 = new Item390Subfield4();
+
+        //act
+        subfield4.decode(input, offset, input.length);
+
+        //assert
+        assertEquals(1, subfield4.getTrafficType());
+        assertEquals(0, subfield4.getFlightRules());
+        assertEquals(0, subfield4.getRvsm());
+        assertEquals(0, subfield4.getPriority());
+    }
+}
