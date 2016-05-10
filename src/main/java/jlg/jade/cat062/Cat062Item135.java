68728c3d60604cbd5ab9f678db5e0b6e8ac87df6
Dan Geabunea <dan.geabunea@jlg.ro>
2016-05-10 13:39:14 +0300

    Fixed two complemente representaiton of item 135

--- src/main/java/jlg/jade/cat062/Cat062Item135.java
+++ src/main/java/jlg/jade/cat062/Cat062Item135.java
@@ -36,11 +36,24 @@ public class Cat062Item135 extends FixedLengthAsterixData {
         appendItemDebugMsg("QNH correction applied", this.qnhCorrectionApplied);
 
         //decode barometric alt
-        byte firstOctetValueWithoutQnh = input[offset];
+        /**
+         * @implNote
+         * - We need to substract 128 if MSB is 1 (QNH)
+         * - We need to generate two complment if bit[6] = 1 (sign)
+         */
+        byte firstOctetValueForCalculation = input[offset];
         if (qnhCorrectionApplied) {
-            firstOctetValueWithoutQnh = (byte) (input[offset] - 128);
+            firstOctetValueForCalculation = (byte) (input[offset] - 128);
         }
-        this.barometricAltitude = firstOctetValueWithoutQnh * 256 + Byte.toUnsignedInt(input[offset + 1]);
+
+        final int SIGN_BIT_INDEX = 6;
+        if (firstOctetBits.get(SIGN_BIT_INDEX)) {
+            //bit set to 1 = > we need two complement to represent negative (number - 2 pow N where N is
+            //nb of bits (in our case 7))
+            firstOctetValueForCalculation = (byte) (firstOctetValueForCalculation - 128);
+        }
+
+        this.barometricAltitude = firstOctetValueForCalculation * 256 + Byte.toUnsignedInt(input[offset + 1]);
         appendItemDebugMsg("Barometric altitude", barometricAltitude);
         appendItemDebugMsg("Barometric altitude (ft)", getBarometricAltitudeFeet());
     }
