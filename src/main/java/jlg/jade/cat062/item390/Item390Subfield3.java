c0e544628d127b5a3c10ab8ef4963756071943fc
Dan Geabunea <dan.geabunea@jlg.ro>
2016-05-10 14:46:38 +0300

    Implemented cat 062 item 390 subfield 3

---
+++ src/main/java/jlg/jade/cat062/item390/Item390Subfield3.java
@@ -0,0 +1,24 @@
+/* 
+* Created by dan-geabunea on 5/10/2016.
+* This code is the property of JLG Consulting. Please
+* check the license terms for this product to see under what
+* conditions you can use or modify this source code.
+*/
+package jlg.jade.cat062.item390;
+
+import jlg.jade.abstraction.FixedLengthAsterixData;
+import jlg.jade.asterix.AsterixItemLength;
+import jlg.jade.asterix.DecodingNotImplemented;
+
+public class Item390Subfield3 extends FixedLengthAsterixData {
+    @Override
+    protected int setSizeInBytes() {
+        return AsterixItemLength.FOUR_BYTES.getValue();
+    }
+
+    @Override
+    @DecodingNotImplemented(reason = "Not needed yet")
+    protected void decodeFromByteArray(byte[] input, int offset) {
+        appendNotImplementedMsg();
+    }
+}
