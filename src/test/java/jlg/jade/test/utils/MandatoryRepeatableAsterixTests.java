2342aa940bc690ea039c68e3ed25a52a1d8cb422
Dan Geabunea <dan.geabunea@jlg.ro>
2016-05-11 18:16:07 +0300

    Implemented decoding for item 390 subfield 12; created abstract class for mandatory rep items test cases

---
+++ src/test/java/jlg/jade/test/utils/MandatoryRepeatableAsterixTests.java
@@ -0,0 +1,81 @@
+/* 
+* Created by dan-geabunea on 5/11/2016.
+* This code is the property of JLG Consulting. Please
+* check the license terms for this product to see under what
+* conditions you can use or modify this source code.
+*/
+package jlg.jade.test.utils;
+
+import jlg.jade.abstraction.RepeatableAsterixData;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public abstract class MandatoryRepeatableAsterixTests<T extends RepeatableAsterixData> {
+    private final int expectedRepeatableBlockSize;
+    private T asterixDataFieldInstance;
+    private final byte[] defaultInputData;
+
+    protected MandatoryRepeatableAsterixTests() {
+        this.expectedRepeatableBlockSize = setExpectedRepeatableBlockSizeInBytes();
+        this.defaultInputData = new byte[1 + this.expectedRepeatableBlockSize];
+        final int repFactor = 1;
+        this.defaultInputData[0] = repFactor;
+    }
+
+    @Before
+    public void init() {
+        asterixDataFieldInstance = setRepeatableAsterixDataInstance();
+        if (asterixDataFieldInstance == null) {
+            throw new RuntimeException("Invalid test exception. Asterix data item instance is null. Did you " +
+                    "initialize it correctly in the test suite class?");
+        }
+    }
+
+    @Test
+    public void repeatable_block_size_should_be_correct() {
+        assertEquals("Repeatable Asterix data field has invalid rep block size", expectedRepeatableBlockSize,
+                asterixDataFieldInstance.getRepeatableBlockSizeInBytes());
+    }
+
+    @Test
+    public void the_decode_method_should_calculate_correct_size() {
+        //arrange
+        int offset = 0;
+
+        //act
+        int result = asterixDataFieldInstance.decode(defaultInputData, offset, defaultInputData.length);
+
+        //assert
+        assertEquals("Repeatable Asterix data field has invalid total size", expectedRepeatableBlockSize + 1,
+                asterixDataFieldInstance.getSizeInBytes());
+    }
+
+    @Test
+    public void the_decode_method_should_increment_offset_after_decoding_data() {
+        //arrange
+        int offset = 0;
+
+        //act
+        int result = asterixDataFieldInstance.decode(defaultInputData, offset, defaultInputData.length);
+
+        //assert
+        assertEquals("Offset not incremented after decoding data", offset + expectedRepeatableBlockSize + 1, result);
+    }
+
+
+    /**
+     * Set the expected and correct size of the repeatable block in bytes.
+     */
+    protected abstract int setExpectedRepeatableBlockSizeInBytes();
+
+    /**
+     * Instantiates a new repeatable Asterix data item that shall be used in the mandatory test suite.
+     */
+    protected abstract T setRepeatableAsterixDataInstance();
+
+    public int getExpectedRepeatableBlockSize() {
+        return expectedRepeatableBlockSize;
+    }
+}
