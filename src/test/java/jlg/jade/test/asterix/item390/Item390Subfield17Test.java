496c1926f5c0fc66469c0735dd2b736c58fb4b83
Dan Geabunea <dan.geabunea@jlg.ro>
2016-05-12 08:27:57 +0300

    Implemented decoding for item 390 subfield 17

---
+++ src/test/java/jlg/jade/test/asterix/item390/Item390Subfield17Test.java
@@ -0,0 +1,22 @@
+/* 
+* Created by dan-geabunea on 5/12/2016.
+* This code is the property of JLG Consulting. Please
+* check the license terms for this product to see under what
+* conditions you can use or modify this source code.
+*/
+package jlg.jade.test.asterix.item390;
+
+import jlg.jade.cat062.item390.Item390Subfield17;
+import jlg.jade.test.utils.MandatoryFixedLengthAsterixTests;
+
+public class Item390Subfield17Test extends MandatoryFixedLengthAsterixTests<Item390Subfield17> {
+    @Override
+    protected int setExpectedItemSizeInBytes() {
+        return 2;
+    }
+
+    @Override
+    protected Item390Subfield17 setFixedLengthAsterixDataInstance() {
+        return new Item390Subfield17();
+    }
+}
