b293bc8b7d1279ba1398772986adbfde19c62329
Dan Geabunea <dan.geabunea@jlg.ro>
2016-05-12 11:10:03 +0300

    Implemented partial decoding og item110 subfield 6

---
+++ src/test/java/jlg/jade/test/asterix/item110/Item110Subfield6Test.java
@@ -0,0 +1,22 @@
+/* 
+* Created by dan-geabunea on 5/12/2016.
+* This code is the property of JLG Consulting. Please
+* check the license terms for this product to see under what
+* conditions you can use or modify this source code.
+*/
+package jlg.jade.test.asterix.item110;
+
+import jlg.jade.cat062.item110.Item110Subfield6;
+import jlg.jade.test.utils.MandatoryFixedLengthAsterixTests;
+
+public class Item110Subfield6Test extends MandatoryFixedLengthAsterixTests<Item110Subfield6> {
+    @Override
+    protected int setExpectedItemSizeInBytes() {
+        return 1;
+    }
+
+    @Override
+    protected Item110Subfield6 setFixedLengthAsterixDataInstance() {
+        return new Item110Subfield6();
+    }
+}
