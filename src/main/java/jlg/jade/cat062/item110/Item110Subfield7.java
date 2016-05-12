74714b79adb4cd5c226c750f719349b6189f06a0
Dan Geabunea <dan.geabunea@jlg.ro>
2016-05-12 11:12:42 +0300

    Implemented partial decoding of item 110 subfield 7

---
+++ src/main/java/jlg/jade/cat062/item110/Item110Subfield7.java
@@ -0,0 +1,27 @@
+/* 
+* Created by dan-geabunea on 5/12/2016.
+* This code is the property of JLG Consulting. Please
+* check the license terms for this product to see under what
+* conditions you can use or modify this source code.
+*/
+package jlg.jade.cat062.item110;
+
+import jlg.jade.abstraction.FixedLengthAsterixData;
+import jlg.jade.asterix.AsterixItemLength;
+import jlg.jade.asterix.DecodingNotImplemented;
+
+/**
+ * Item 110 Subfield 7 - X Pulse Presence
+ */
+public class Item110Subfield7 extends FixedLengthAsterixData {
+    @Override
+    protected int setSizeInBytes() {
+        return AsterixItemLength.ONE_BYTE.getValue();
+    }
+
+    @Override
+    @DecodingNotImplemented(reason = "Subfield not needed yet")
+    protected void decodeFromByteArray(byte[] input, int offset) {
+        appendNotImplementedMsg();
+    }
+}
