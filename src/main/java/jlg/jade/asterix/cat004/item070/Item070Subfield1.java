df70b7d5a8368d4d55719bcf758b4c678ed39882
Dan Geabunea <dan.geabunea@jlg.ro>
2016-06-03 15:29:19 +0300

    Finished decoding of cat 004 item 070 subfield 1

---
+++ src/main/java/jlg/jade/asterix/cat004/item070/Item070Subfield1.java
@@ -0,0 +1,20 @@
+/* 
+* Created by dan-geabunea on 6/3/2016.
+* This code is the property of JLG Consulting. Please
+* check the license terms for this product to see under what
+* conditions you can use or modify this source code.
+*/
+package jlg.jade.asterix.cat004.item070;
+
+import jlg.jade.asterix.AsterixTimeData;
+
+/**
+ * Cat 004 - Item 070 - Subfield 1 - Time to conflict
+ * Unit of measure: 1/128 sec
+ */
+public class Item070Subfield1 extends AsterixTimeData{
+    @Override
+    protected String setDisplayName() {
+        return "Item070Subfield1 - Time to Conflict";
+    }
+}
---
+++ src/test/java/jlg/jade/test/asterix/cat004/item070/Item070Subfield1Test.java
@@ -0,0 +1,39 @@
+/* 
+* Created by dan-geabunea on 6/3/2016.
+* This code is the property of JLG Consulting. Please
+* check the license terms for this product to see under what
+* conditions you can use or modify this source code.
+*/
+package jlg.jade.test.asterix.cat004.item070;
+
+import jlg.jade.asterix.cat004.item070.Item070Subfield1;
+import jlg.jade.test.utils.MandatoryFixedLengthAsterixTests;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Item070Subfield1Test extends MandatoryFixedLengthAsterixTests<Item070Subfield1> {
+    @Override
+    protected int setExpectedItemSizeInBytes() {
+        return 3;
+    }
+
+    @Override
+    protected Item070Subfield1 setFixedLengthAsterixDataInstance() {
+        return new Item070Subfield1();
+    }
+
+    @Test
+    public void the_decode_method_should_correctly_decode_data(){
+        //arrange
+        byte[] input = {103, (byte) 172, (byte) 233};
+        int offset = 0;
+        Item070Subfield1 subfield1 = new Item070Subfield1();
+
+        //act
+        subfield1.decode(input, offset, input.length);
+
+        //assert
+        assertEquals("Time information not decoded correctly", 6794473, subfield1.getTime());
+    }
+}
