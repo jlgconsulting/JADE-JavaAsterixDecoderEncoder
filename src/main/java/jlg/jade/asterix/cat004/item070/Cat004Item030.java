7bc97b2c04a8d3cd623c003ce45dbe2175164d42
Dan Geabunea <dan.geabunea@jlg.ro>
2016-06-22 14:38:41 +0300

    Implemented decoding for cat004 item030;

---
+++ src/main/java/jlg/jade/asterix/cat004/item070/Cat004Item030.java
@@ -0,0 +1,38 @@
+package jlg.jade.asterix.cat004.item070;
+
+import jlg.jade.asterix.AsterixItemLength;
+import jlg.jade.asterix.FixedLengthAsterixData;
+import jlg.jade.common.UnsignedNumericDecoder;
+
+/**
+ * Cat 004 Item 030 - Track Number 1
+ * This is the track number of the first track involved in the conflict in case
+ * of an STCA or a RIMCA or the track involved in case of one of the other
+ * Safety Net functions.
+ * This track number is distributed in this field exactly as it was received
+ * from the Radar Processor Unit (identified by I004/015) and its range is
+ * depending on the range used by that unit
+ */
+public class Cat004Item030 extends FixedLengthAsterixData {
+    private int trackNumber;
+
+    @Override
+    protected int setSizeInBytes() {
+        return AsterixItemLength.TWO_BYTES.getValue();
+    }
+
+    @Override
+    protected void decodeFromByteArray(byte[] input, int offset) {
+        this.trackNumber = UnsignedNumericDecoder.decodeFromTwoBytes(input,offset);
+        appendItemDebugMsg("Track Number", this.trackNumber);
+    }
+
+    @Override
+    protected String setDisplayName() {
+        return "Cat004Item030 - Track Number 1";
+    }
+
+    public int getTrackNumber() {
+        return trackNumber;
+    }
+}
---
+++ src/test/java/jlg/jade/test/asterix/cat004/Cat004Item030Test.java
@@ -0,0 +1,36 @@
+package jlg.jade.test.asterix.cat004;
+
+import jlg.jade.asterix.cat004.item070.Cat004Item030;
+import jlg.jade.test.utils.MandatoryFixedLengthAsterixTests;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Dan Geabunea on 6/22/2016.
+ */
+public class Cat004Item030Test extends MandatoryFixedLengthAsterixTests<Cat004Item030> {
+    @Override
+    protected int setExpectedItemSizeInBytes() {
+        return 2;
+    }
+
+    @Override
+    protected Cat004Item030 setFixedLengthAsterixDataInstance() {
+        return new Cat004Item030();
+    }
+
+    @Test
+    public void the_decode_method_should_correctly_decode_data(){
+        //arrange
+        byte[] input = {20, 55};
+        int offset = 0;
+        Cat004Item030 item030 = new Cat004Item030();
+
+        //act
+        item030.decode(input, offset, input.length);
+
+        //assert
+        assertEquals("Data not decoded correctly", 5175, item030.getTrackNumber());
+    }
+}
