c9d108f269333ba5bb223af62775283c3893cd3c
Dan Geabunea <dan.geabunea@jlg.ro>
2016-06-23 09:13:01 +0300

    Implemented decoding of cat004 item 170/171 subfield 5

---
+++ src/main/java/jlg/jade/asterix/cat004/Cat004AircraftCharacteristicsSubfield5.java
@@ -0,0 +1,18 @@
+package jlg.jade.asterix.cat004;
+
+import jlg.jade.asterix.AsterixTimeData;
+
+/**
+ * Cat 004 - Item 170/171 - Subfield 5 - Time to treshold
+ * Time to runway threshold for approaching aircraft in a
+ * RIMCAS
+ *
+ * @implSpec Time to Threshold expressed in Two’s Complement
+ * @implSpec Unit of measure is 1/128 sec
+ */
+public class Cat004AircraftCharacteristicsSubfield5 extends AsterixTimeData {
+    @Override
+    protected String setDisplayName() {
+        return "Subfield5 - Time to Treshlod RIMCAS";
+    }
+}
---
+++ src/test/java/jlg/jade/test/asterix/cat004/item170_171/Cat004AircraftCharacteristicsSubfield5Test.java
@@ -0,0 +1,21 @@
+package jlg.jade.test.asterix.cat004.item170_171;
+
+import jlg.jade.asterix.cat004.Cat004AircraftCharacteristicsSubfield5;
+import jlg.jade.test.utils.MandatoryFixedLengthAsterixTests;
+
+public class Cat004AircraftCharacteristicsSubfield5Test extends
+        MandatoryFixedLengthAsterixTests<Cat004AircraftCharacteristicsSubfield5> {
+    @Override
+    protected int setExpectedItemSizeInBytes() {
+        return 3;
+    }
+
+    @Override
+    protected Cat004AircraftCharacteristicsSubfield5 setFixedLengthAsterixDataInstance() {
+        return new Cat004AircraftCharacteristicsSubfield5();
+    }
+
+    /**
+     * Decoding is tested in base class
+     */
+}
