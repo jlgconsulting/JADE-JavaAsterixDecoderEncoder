efc36695ad3b50fa5e662056a089e91b607c5e1d
Alexandru Ionescu <alexandru.ionescu@jlg.ro>
2016-07-12 15:11:07 +0300

    Added counter and test for Cat034

---
+++ src/main/java/jlg/jade/asterix/counters/Cat034ItemCounter.java
@@ -0,0 +1,312 @@
+/**
+ * Created by alexandru on 7/12/16.
+ */
+package jlg.jade.asterix.counters;
+
+import jlg.jade.asterix.AsterixDataBlock;
+import jlg.jade.asterix.AsterixRecord;
+import jlg.jade.asterix.cat034.Cat034Record;
+import jlg.jade.common.DebugMessageSource;
+
+public class Cat034ItemCounter extends DebugMessageSource implements AsterixItemCounter {
+
+    private int nbRecords;
+    private int nbOfInvalidRecords;
+
+    private int item010Present;
+    private int item010Invalid;
+    private int item000Present;
+    private int item000Invalid;
+    private int item030Present;
+    private int item030Invalid;
+    private int item020Present;
+    private int item020Invalid;
+    private int item041Present;
+    private int item041Invalid;
+    private int item050Present;
+    private int item050Invalid;
+    private int item060Present;
+    private int item060Invalid;
+    private int item070Present;
+    private int item070Invalid;
+    private int item100Present;
+    private int item100Invalid;
+    private int item110Present;
+    private int item110Invalid;
+    private int item120Present;
+    private int item120Invalid;
+    private int item090Present;
+    private int item090Invalid;
+    private int itemRePresent;
+    private int itemReInvalid;
+    private int itemSpPresent;
+    private int itemSpInvalid;
+
+    /**
+     * Increments counter values with the ones received from a new data block
+     *
+     * @param asterixDataBlock represents an Asterix data block with has Cat 034 data
+     */
+    @Override
+    public void increment(AsterixDataBlock asterixDataBlock) {
+        for (AsterixRecord record : asterixDataBlock.getRecords()) {
+            if (record.getCat034Record() != null) {
+                increment(record.getCat034Record());
+            }
+        }
+    }
+
+    /**
+     * Increments the existing counter with values from the Cat 034 record
+     *
+     * @param record represents a new Asterix record
+     */
+    private void increment(Cat034Record record) {
+        clearDebugMsg();
+        appendNewLine();
+        appendItemCounterMsg("Asterix Category 034 Counters", "Nb. decoded", "Nb. invalid");
+
+        this.nbRecords++;
+        if (!record.isValid()) {
+            this.nbOfInvalidRecords++;
+        }
+        appendItemCounterMsg("Number of records", nbRecords, nbOfInvalidRecords);
+
+        if (record.getItem010() != null) {
+            this.item010Present += 1;
+            if (!record.getItem010().isValid()) {
+                this.item010Invalid += 1;
+            }
+            appendItemCounterMsg("Item 010 - Data Source Identifier", this.item010Present,
+                                 this.item010Invalid);
+        }
+
+        if (record.getItem000() != null) {
+            this.item000Present += 1;
+            if (!record.getItem000().isValid()) {
+                this.item000Invalid += 1;
+            }
+            appendItemCounterMsg("Item 000 - Message Type", this.item000Present,
+                                 this.item000Invalid);
+        }
+
+        if (record.getItem030() != null) {
+            this.item030Present += 1;
+            if (!record.getItem030().isValid()) {
+                this.item030Invalid += 1;
+            }
+            appendItemCounterMsg("Item 030 - Time of Day", this.item030Present,
+                                 this.item030Invalid);
+        }
+
+        if (record.getItem020() != null) {
+            this.item020Present += 1;
+            if (!record.getItem020().isValid()) {
+                this.item020Invalid += 1;
+            }
+            appendItemCounterMsg("Item 020 - Sector Number", this.item020Present,
+                                 this.item020Invalid);
+        }
+
+        if (record.getItem041() != null) {
+            this.item041Present += 1;
+            if (!record.getItem041().isValid()) {
+                this.item041Invalid += 1;
+            }
+            appendItemCounterMsg("Item 041 - Antenna Rotation Speed", this.item041Present,
+                                 this.item041Invalid);
+        }
+
+        if (record.getItem050() != null) {
+            this.item050Present += 1;
+            if (!record.getItem050().isValid()) {
+                this.item050Invalid += 1;
+            }
+            appendItemCounterMsg("Item 050 - System Configuration and Status", this.item050Present,
+                                 this.item050Invalid);
+        }
+
+        if (record.getItem060() != null) {
+            this.item060Present += 1;
+            if (!record.getItem060().isValid()) {
+                this.item060Invalid += 1;
+            }
+            appendItemCounterMsg("Item 060 - System Processing Mode", this.item060Present,
+                                 this.item060Invalid);
+        }
+
+        if (record.getItem070() != null) {
+            this.item070Present += 1;
+            if (!record.getItem070().isValid()) {
+                this.item070Invalid += 1;
+            }
+            appendItemCounterMsg("Item 070 - Message Count Values", this.item070Present,
+                                 this.item070Invalid);
+        }
+
+        if (record.getItem100() != null) {
+            this.item100Present += 1;
+            if (!record.getItem100().isValid()) {
+                this.item100Invalid += 1;
+            }
+            appendItemCounterMsg("Item 100 - Generic Polar Window", this.item100Present,
+                                 this.item100Invalid);
+        }
+
+        if (record.getItem110() != null) {
+            this.item110Present += 1;
+            if (!record.getItem110().isValid()) {
+                this.item110Invalid += 1;
+            }
+            appendItemCounterMsg("Item 110 - Data Filter", this.item110Present,
+                                 this.item110Invalid);
+        }
+
+        if (record.getItem120() != null) {
+            this.item120Present += 1;
+            if (!record.getItem120().isValid()) {
+                this.item120Invalid += 1;
+            }
+            appendItemCounterMsg("Item 120 - 3D-Position Of Data Source", this.item120Present,
+                                 this.item120Invalid);
+        }
+
+        if (record.getItem090() != null) {
+            this.item090Present += 1;
+            if (!record.getItem090().isValid()) {
+                this.item090Invalid += 1;
+            }
+            appendItemCounterMsg("Item 090 - Collimation Error", this.item090Present,
+                                 this.item090Invalid);
+        }
+
+        if (record.getReservedExpansionField() != null) {
+            this.itemRePresent += 1;
+            if (!record.getReservedExpansionField().isValid()) {
+                this.itemReInvalid += 1;
+            }
+            appendItemCounterMsg("RE", this.itemRePresent, this.itemReInvalid);
+        }
+
+        if (record.getSpecialPurposeField() != null) {
+            this.itemSpPresent += 1;
+            if (!record.getSpecialPurposeField().isValid()) {
+                this.itemSpInvalid += 1;
+            }
+            appendItemCounterMsg("SP", this.itemSpPresent, this.itemSpInvalid);
+        }
+
+    }
+
+    public int getItem010Present() {
+        return item010Present;
+    }
+
+    public int getItem010Invalid() {
+        return item010Invalid;
+    }
+
+    public int getItem000Present() {
+        return item000Present;
+    }
+
+    public int getItem000Invalid() {
+        return item000Invalid;
+    }
+
+    public int getItem030Present() {
+        return item030Present;
+    }
+
+    public int getItem030Invalid() {
+        return item030Invalid;
+    }
+
+    public int getItem020Present() {
+        return item020Present;
+    }
+
+    public int getItem020Invalid() {
+        return item020Invalid;
+    }
+
+    public int getItem041Present() {
+        return item041Present;
+    }
+
+    public int getItem041Invalid() {
+        return item041Invalid;
+    }
+
+    public int getItem050Present() {
+        return item050Present;
+    }
+
+    public int getItem050Invalid() {
+        return item050Invalid;
+    }
+
+    public int getItem060Present() {
+        return item060Present;
+    }
+
+    public int getItem060Invalid() {
+        return item060Invalid;
+    }
+
+    public int getItem070Present() {
+        return item070Present;
+    }
+
+    public int getItem070Invalid() {
+        return item070Invalid;
+    }
+
+    public int getItem100Present() {
+        return item100Present;
+    }
+
+    public int getItem100Invalid() {
+        return item100Invalid;
+    }
+
+    public int getItem110Present() {
+        return item110Present;
+    }
+
+    public int getItem110Invalid() {
+        return item110Invalid;
+    }
+
+    public int getItem120Present() {
+        return item120Present;
+    }
+
+    public int getItem120Invalid() {
+        return item120Invalid;
+    }
+
+    public int getItem090Present() {
+        return item090Present;
+    }
+
+    public int getItem090Invalid() {
+        return item090Invalid;
+    }
+
+    public int getItemRePresent() {
+        return itemRePresent;
+    }
+
+    public int getItemReInvalid() {
+        return itemReInvalid;
+    }
+
+    public int getItemSpPresent() {
+        return itemSpPresent;
+    }
+
+    public int getItemSpInvalid() {
+        return itemSpInvalid;
+    }
+}
---
+++ src/test/java/jlg/jade/test/asterix/counters/Cat034ItemCounterTest.java
@@ -0,0 +1,65 @@
+/**
+ * Created by alexandru on 7/12/16.
+ */
+package jlg.jade.test.asterix.counters;
+
+import jlg.jade.asterix.AsterixDataBlock;
+import jlg.jade.asterix.AsterixRecord;
+import jlg.jade.asterix.cat034.Cat034Item000;
+import jlg.jade.asterix.cat034.Cat034Item010;
+import jlg.jade.asterix.cat034.Cat034Item030;
+import jlg.jade.asterix.cat034.Cat034Record;
+import jlg.jade.asterix.counters.Cat034ItemCounter;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+public class Cat034ItemCounterTest {
+
+    @Test
+    public void the_item_counter_should_correctly_count_present_items() {
+        // arrange
+        Cat034ItemCounter counter = new Cat034ItemCounter();
+        Cat034Record cat034RecordMock = Mockito.mock(Cat034Record.class);
+        AsterixDataBlock adbMock = Mockito.mock(AsterixDataBlock.class);
+
+        List<AsterixRecord> asterixRecordList = new ArrayList<>();
+        AsterixRecord asterixRecordOne = Mockito.mock(AsterixRecord.class);
+
+        asterixRecordList.add(asterixRecordOne);
+
+        // act
+
+        // add items to mocked cat 034 record
+        when(cat034RecordMock.getItem010()).thenReturn(new Cat034Item010());
+        when(cat034RecordMock.getItem000()).thenReturn(new Cat034Item000());
+        when(cat034RecordMock.getItem030()).thenReturn(new Cat034Item030());
+
+        // mock the asterixRecordOne getCat034Record() to return our cat 034 record mock
+        when(asterixRecordOne.getCat034Record()).thenReturn(cat034RecordMock);
+
+        // mock the Asterix data block getRecords() method to return the list with our AsterixRecord
+        when(adbMock.getRecords()).thenReturn(asterixRecordList);
+
+        counter.increment(adbMock);
+
+        // assert
+        assertEquals("Number of Item 010 in counter should be 1", 1, counter.getItem010Present());
+        assertEquals("Number of invalid item 010 in counter should be 0", 0,
+                     counter.getItem010Invalid());
+
+        assertEquals("Number of Item 000 in counter should be 1", 1, counter.getItem000Present());
+        assertEquals("Number of invalid item 010 in counter should be 0", 0,
+                     counter.getItem000Invalid());
+
+        assertEquals("Number of Item 030 in counter should be 1", 1, counter.getItem030Present());
+        assertEquals("Number of invalid item 030 in counter should be 0", 0,
+                     counter.getItem030Invalid());
+
+    }
+}
