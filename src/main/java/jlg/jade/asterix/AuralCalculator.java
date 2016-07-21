9dadd25b70239efb1501d677e6016037ce40ba39
Alexandru Ionescu <alexandru.ionescu@jlg.ro>
2016-07-21 16:43:14 +0300

    Added ARA list decoding for TCAS v6.04

--- src/main/java/jlg/jade/asterix/AuralCalculator.java
+++ src/main/java/jlg/jade/asterix/AuralCalculator.java
@@ -3,16 +3,48 @@
  */
 package jlg.jade.asterix;
 
+import java.util.ArrayList;
 import java.util.BitSet;
 import java.util.HashMap;
+import java.util.List;
 
 public class AuralCalculator {
 
     private static AuralCalculator auralCalculatorInstance = null;
 
     private static String auralCode;
+    private static List<String> araList = new ArrayList<>();
 
     static HashMap<String, String> Auralv604RepresentationToCodeDictionary;
+
+    // TODO: Should we process the bitset in order to have the bit indexes like in the Cat048Item260 specification document?
+    // MTI / MTE bit index and value
+    static final int MTI_BIT_INDEX = 28;
+    static int MTIBitValue = 0;
+
+    // ARA bits indexes
+    static final int ARA_BIT41_INDEX = 15;
+    static final int ARA_BIT42_INDEX = 14;
+    static final int ARA_BIT43_INDEX = 13;
+    static final int ARA_BIT44_INDEX = 12;
+    static final int ARA_BIT45_INDEX = 11;
+    static final int ARA_BIT46_INDEX = 10;
+    static final int ARA_BIT47_INDEX = 9;
+    static final int ARA_BIT48_INDEX = 8;
+    static final int ARA_BIT49_INDEX = 23;
+    static final int ARA_BIT50_INDEX = 22;
+
+    // initialize ARA bits to 0
+    static int ARABit41Value = 0;
+    static int ARABit42Value = 0;
+    static int ARABit43Value = 0;
+    static int ARABit44Value = 0;
+    static int ARABit45Value = 0;
+    static int ARABit46Value = 0;
+    static int ARABit47Value = 0;
+    static int ARABit48Value = 0;
+    static int ARABit49Value = 0;
+    static int ARABit50Value = 0;
 
     /**
      * TODO: Move the implementation of the 2 sets of aurals ( for 6.04 and 7.0 / 7.1 ) to different classes
@@ -78,6 +110,7 @@ public class AuralCalculator {
 
         switch (version) {
             case VERSION_604:
+                setARABits(acasInputBits);
                 auralCode = getVersion604AuralCode(acasInputBits);
                 break;
         }
@@ -95,35 +128,77 @@ public class AuralCalculator {
      * @return the auralCode code
      */
     private static String getVersion604AuralCode(BitSet acasInputBits) {
-        // TODO: Should we process the bitset in order to have the bit indexes like in the Cat048Item260 specification document?
-        // MTI / MTE bit index and value
-        final int MTI_BIT_INDEX = 28;
-        int MTIBitValue = 0;
+        // calculate String representation of the binary value
+        // represented by the bits MTI - ARA41 - ARA42 - ... - ARA50
+        // and retrieve the code from the specific structure
 
-        // ARA bits indexes
-        final int ARA_BIT41_INDEX = 15;
-        final int ARA_BIT42_INDEX = 14;
-        final int ARA_BIT43_INDEX = 13;
-        final int ARA_BIT44_INDEX = 12;
-        final int ARA_BIT45_INDEX = 11;
-        final int ARA_BIT46_INDEX = 10;
-        final int ARA_BIT47_INDEX = 9;
-        final int ARA_BIT48_INDEX = 8;
-        final int ARA_BIT49_INDEX = 23;
-        final int ARA_BIT50_INDEX = 22;
+        StringBuffer sb = new StringBuffer();
+        sb.append(MTIBitValue).append(ARABit41Value).append(ARABit42Value).append(ARABit43Value)
+                .append(ARABit44Value).append(ARABit45Value).append(ARABit46Value)
+                .append(ARABit47Value).append(ARABit48Value).append(ARABit49Value)
+                .append(ARABit50Value);
+        String auralBinaryRepresentation = sb.toString();
 
-        // initialize ARA bits to 0
-        int ARABit41Value = 0;
-        int ARABit42Value = 0;
-        int ARABit43Value = 0;
-        int ARABit44Value = 0;
-        int ARABit45Value = 0;
-        int ARABit46Value = 0;
-        int ARABit47Value = 0;
-        int ARABit48Value = 0;
-        int ARABit49Value = 0;
-        int ARABit50Value = 0;
+        auralCode = AuralCalculator.getInstance().getAuralv604RepresentationToCodeDictionary()
+                .get(sb.toString());
 
+        return auralCode;
+    }
+
+    /**
+     * TODO: refactor usage of bitset so they are common for the class, not method
+     * TODO: refactor version to switch logic depending on version
+     * @param input
+     * @param version TCAS version to be used
+     * @return list of ARA messages
+     */
+    public static List<String> getAraList(byte[] input, TCASVersion version) {
+        BitSet acasInputBits = BitSet.valueOf(input);
+
+        if (acasInputBits.get(ARA_BIT41_INDEX)) {
+            araList.add("Climb");
+        }
+
+        if (acasInputBits.get(ARA_BIT42_INDEX)) {
+            araList.add("Don’t descend");
+        }
+
+        if (acasInputBits.get(ARA_BIT43_INDEX)) {
+            araList.add("Don’t descend faster than 500 fpm");
+        }
+
+        if (acasInputBits.get(ARA_BIT44_INDEX)) {
+            araList.add("Don’t descend faster than 1000 fpm");
+        }
+
+        if (acasInputBits.get(ARA_BIT45_INDEX)) {
+            araList.add("Don’t descend faster than 2000 fpm");
+        }
+
+        if (acasInputBits.get(ARA_BIT46_INDEX)) {
+            araList.add("Descend");
+        }
+
+        if(acasInputBits.get(ARA_BIT47_INDEX)) {
+            araList.add("Don’t climb");
+        }
+
+        if(acasInputBits.get(ARA_BIT48_INDEX)) {
+            araList.add("Don’t climb faster than 500 fpm");
+        }
+
+        if(acasInputBits.get(ARA_BIT49_INDEX)) {
+            araList.add("Don’t climb faster than 1000 fpm");
+        }
+
+        if(acasInputBits.get(ARA_BIT50_INDEX)) {
+            araList.add("Don’t climb faster than 2000 fpm");
+        }
+
+        return araList;
+    }
+
+    private static void setARABits(BitSet acasInputBits) {
         // TODO: find a better solution to check these bits
         // determine the value of each bit
         if (acasInputBits.get(MTI_BIT_INDEX)) {
@@ -169,21 +244,5 @@ public class AuralCalculator {
         if (acasInputBits.get(ARA_BIT50_INDEX)) {
             ARABit50Value = 1;
         }
-
-        // calculate String representation of the binary value
-        // represented by the bits MTI - ARA41 - ARA42 - ... - ARA50
-        // and retrieve the code from the specific structure
-
-        StringBuffer sb = new StringBuffer();
-        sb.append(MTIBitValue).append(ARABit41Value).append(ARABit42Value).append(ARABit43Value)
-                .append(ARABit44Value).append(ARABit45Value).append(ARABit46Value)
-                .append(ARABit47Value).append(ARABit48Value).append(ARABit49Value)
-                .append(ARABit50Value);
-        String auralBinaryRepresentation = sb.toString();
-
-        auralCode = AuralCalculator.getInstance().getAuralv604RepresentationToCodeDictionary()
-                .get(sb.toString());
-
-        return auralCode;
     }
 }
--- src/test/java/jlg/jade/test/asterix/AuralCalculatorTest.java
+++ src/test/java/jlg/jade/test/asterix/AuralCalculatorTest.java
@@ -7,7 +7,12 @@ import jlg.jade.asterix.AuralCalculator;
 import jlg.jade.asterix.TCASVersion;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class AuralCalculatorTest {
 
@@ -22,5 +27,18 @@ public class AuralCalculatorTest {
         // assert
         assertEquals("Aural code is incorrect!", "CCC, or CXC-CXC, or IC-IC, or CCN-CCN",
                      auralCode);
+    }
+
+    @Test
+    public void the_aural_calculator_should_correcly_calculate_ara_list_for_version604() {
+        // arrange
+        byte[] input = {48, (byte) 128, 0, 0, 0, 0, 0};
+        List<String> expectedAraList = new ArrayList<>(Arrays.asList("Climb"));
+
+        // act
+        List<String> araList = AuralCalculator.getAraList(input, TCASVersion.VERSION_604);
+
+        // assert
+        assertTrue("ARA list does not match!", expectedAraList.equals(araList));
     }
 }
