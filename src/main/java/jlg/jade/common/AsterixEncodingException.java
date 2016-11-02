dc5c306b1f66fc951782b7ade52a509aa4e38a9d
Dan Geabunea <dan.geabunea@jlg.ro>
2016-11-02 14:38:18 +0200

    All data source items (sac sic) can be encoded (finished)

---
+++ src/main/java/jlg/jade/common/AsterixEncodingException.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Dan Geabunea on 11/2/2016.
+ */
+
+package jlg.jade.common;
+
+public class AsterixEncodingException extends Exception {
+    public AsterixEncodingException(String msg) {
+        super(msg);
+    }
+}
