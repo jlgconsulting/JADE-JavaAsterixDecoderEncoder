7da2a7e0d588bb74ca4175f7dd01d040b82943a8
Alexandru Ionescu <alexandru.ionescu@jlg.ro>
2017-10-02 12:46:51 +0300

    [ASMT-1130] Added decoding for Cat004Item100Subfield1 - Area Name

--- src/main/java/jlg/jade/asterix/cat004/item100/Cat004Item100Subfield1.java
+++ src/main/java/jlg/jade/asterix/cat004/item100/Cat004Item100Subfield1.java
@@ -7,6 +7,7 @@ package jlg.jade.asterix.cat004.item100;
 import jlg.jade.asterix.AsterixItemLength;
 import jlg.jade.asterix.DecodingNotImplemented;
 import jlg.jade.asterix.FixedLengthAsterixData;
+import jlg.jade.common.IA5Decoder;
 
 /**
  * Cat004 Item100 Subfield1 - Area Name
@@ -16,19 +17,25 @@ import jlg.jade.asterix.FixedLengthAsterixData;
  * characters are filled with space character
  */
 public class Cat004Item100Subfield1 extends FixedLengthAsterixData {
+    private String areaName;
+
     @Override
     protected int setSizeInBytes() {
         return AsterixItemLength.SIX_BYTES.getValue();
     }
 
     @Override
-    @DecodingNotImplemented(reason = "Item not needed yet")
     protected void decodeFromByteArray(byte[] input, int offset) {
-        appendNotImplementedMsg();
+        this.areaName = IA5Decoder.decodeFromSixBytes(input, offset);
+        appendItemDebugMsg("Area Name", this.areaName);
     }
 
     @Override
     protected String setDisplayName() {
         return "Cat004Item100Subfield1 - Area Name";
+    }
+
+    public String getAreaName() {
+        return areaName;
     }
 }
--- src/test/java/jlg/jade/test/asterix/cat004/item100/Cat004Item100Subfield1Test.java
+++ src/test/java/jlg/jade/test/asterix/cat004/item100/Cat004Item100Subfield1Test.java
@@ -5,7 +5,11 @@
 package jlg.jade.test.asterix.cat004.item100;
 
 import jlg.jade.asterix.cat004.item100.Cat004Item100Subfield1;
+import jlg.jade.common.IA5Decoder;
 import jlg.jade.test.utils.MandatoryFixedLengthAsterixTests;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class Cat004Item100Subfield1Test extends MandatoryFixedLengthAsterixTests<Cat004Item100Subfield1> {
 
@@ -17,5 +21,19 @@ public class Cat004Item100Subfield1Test extends MandatoryFixedLengthAsterixTests
     @Override
     protected Cat004Item100Subfield1 setFixedLengthAsterixDataInstance() {
         return new Cat004Item100Subfield1();
+    }
+
+    @Test
+    public void the_decode_method_should_correctly_decode_data() {
+        // arrange
+        byte[] input = {57, 5, 52, (byte) 203, (byte) 1, (byte) 202};
+        int offset = 0;
+        Cat004Item100Subfield1 item100Subfield1 = new Cat004Item100Subfield1();
+
+        // act
+        item100Subfield1.decode(input, offset, input.length);
+
+        // assert
+        assertEquals("Data not decoded correctly - Area Name", "NPT420GJ", item100Subfield1.getAreaName());
     }
 }
